0f75e2031c954e624dd2698ba14d43089fbd37a1
sartra <strenshaw@gmail.com>
2018-10-02 22:18:58 -0400

    so

---
+++ namespace.ts
@@ -0,0 +1,15 @@
+// namespace MyMath {
+//   const PI = 3.14;
+
+//   // must export funcs so they are available outside of namespace 
+//   export function calculateCircumference(diameter: number) {
+//     return diameter * PI; 
+//   }
+
+//   export function calculateRectangle(width:number, length: number) {
+//     return width * length;
+//   }
+// }
+
+// console.log(MyMath.calculateRectangle(10,20)); 
+// console.log(MyMath.calculateCircumference(3)); 
